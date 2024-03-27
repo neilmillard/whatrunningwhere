03b9cc618e11c68f0aa9a8ecb303380eaf41956d
Neil Millard <neil@neilmillard.com>
2024-03-27 13:54:02 +0000

    add deployment class

--- src/Domain/Deployment/Deployment.php
+++ src/Domain/Deployment/Deployment.php
@@ -2,7 +2,72 @@
 
 namespace App\Domain\Deployment;
 
-class Deployment
-{
+use JsonSerializable;
 
+class Deployment implements JsonSerializable
+{
+    private ?int $id;
+
+    private string $who;
+
+    private string $application;
+
+    private string $version;
+
+    private string $environment;
+
+    private int $time;
+
+    public function __construct(?int $id, string $who, string $application, string $version, string $environment)
+    {
+        $this->id = $id;
+        $this->who = strtolower($who);
+        $this->application = strtolower($application);
+        $this->version = $version;
+        $this->environment = strtolower($environment);
+        $this->time = date("Y-m-d H:i:s");
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWho(): string
+    {
+        return $this->who;
+    }
+
+    public function getApplication(): string
+    {
+        return $this->application;
+    }
+
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    public function getEnvironment(): string
+    {
+        return $this->environment;
+    }
+
+    public function getTime(): int
+    {
+        return $this->time;
+    }
+
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'who' => $this->who,
+            'application' => $this->application,
+            'version' => $this->version,
+            'environment' => $this->environment,
+            'time' => $this->time,
+        ];
+    }
 }
