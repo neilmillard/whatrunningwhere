32ab00b107d3bbaf2c43432571070e8c8232e306
Neil Millard <neil.millard@gmail.com>
2024-08-18 22:34:31 +0100

    Update Deps Add ResponseEmitterTest.php

---
+++ tests/Application/ResponseEmitter/ResponseEmitterTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Application\ResponseEmitter;
+
+use App\Application\ResponseEmitter\ResponseEmitter;
+use Tests\Assets\HeaderStack;
+use Tests\TestCase;
+
+
+class ResponseEmitterTest extends TestCase
+{
+    public function setUp(): void
+    {
+        HeaderStack::reset();
+    }
+
+    public function tearDown(): void
+    {
+        HeaderStack::reset();
+    }
+
+    public function testEmit()
+    {
+        $response = $this->createResponse();
+        $response->getBody()->write('Hello');
+
+        $responseEmitter = new ResponseEmitter();
+        $responseEmitter->emit($response);
+
+        $expectedStack = [
+            ['header' => 'Access-Control-Allow-Credentials: true', 'replace' => true, 'status_code' => null],
+            ['header' => 'Access-Control-Allow-Origin: ', 'replace' => true, 'status_code' => null],
+            ['header' => 'Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization', 'replace' => true, 'status_code' => null],
+            ['header' => 'Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS', 'replace' => true, 'status_code' => null],
+            ['header' => 'Cache-Control: no-store, no-cache, must-revalidate, max-age=0', 'replace' => true, 'status_code' => null],
+            ['header' => 'Cache-Control: post-check=0, pre-check=0', 'replace' => false, 'status_code' => null],
+            ['header' => 'Pragma: no-cache', 'replace' => true, 'status_code' => null],
+            ['header' => 'HTTP/1.1 200 ', 'replace' => true, 'status_code' => 200],
+        ];
+        $this->assertSame($expectedStack, HeaderStack::stack());
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,31 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+use AdrianSuter\Autoload\Override\Override;
+use Slim\ResponseEmitter;
+use Tests\Assets\HeaderStack;
+
+$classLoader = require __DIR__ . '/../vendor/autoload.php';
+
+Override::apply($classLoader, [
+    ResponseEmitter::class => [
+        'connection_status' => function (): int {
+            if (isset($GLOBALS['connection_status_return'])) {
+                return $GLOBALS['connection_status_return'];
+            }
+
+            return connection_status();
+        },
+        'header' => function (string $string, bool $replace = true, int $statusCode = null): void {
+            HeaderStack::push(
+                [
+                    'header' => $string,
+                    'replace' => $replace,
+                    'status_code' => $statusCode,
+                ]
+            );
+        },
+        'headers_sent' => function (): bool {
+            return false;
+        }
+    ]
+]);
