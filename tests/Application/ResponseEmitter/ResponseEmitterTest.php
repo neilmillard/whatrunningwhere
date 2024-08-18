e1e17884362750ca557f3b588b7fa77f86b766dc
Neil Millard <neil.millard@gmail.com>
2024-08-18 22:47:11 +0100

    Formatting

--- tests/Application/ResponseEmitter/ResponseEmitterTest.php
+++ tests/Application/ResponseEmitter/ResponseEmitterTest.php
@@ -6,7 +6,6 @@ use App\Application\ResponseEmitter\ResponseEmitter;
 use Tests\Assets\HeaderStack;
 use Tests\TestCase;
 
-
 class ResponseEmitterTest extends TestCase
 {
     public function setUp(): void
@@ -30,9 +29,12 @@ class ResponseEmitterTest extends TestCase
         $expectedStack = [
             ['header' => 'Access-Control-Allow-Credentials: true', 'replace' => true, 'status_code' => null],
             ['header' => 'Access-Control-Allow-Origin: ', 'replace' => true, 'status_code' => null],
-            ['header' => 'Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization', 'replace' => true, 'status_code' => null],
-            ['header' => 'Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS', 'replace' => true, 'status_code' => null],
-            ['header' => 'Cache-Control: no-store, no-cache, must-revalidate, max-age=0', 'replace' => true, 'status_code' => null],
+            ['header' => 'Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization',
+                'replace' => true, 'status_code' => null],
+            ['header' => 'Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS', 'replace' => true,
+                'status_code' => null],
+            ['header' => 'Cache-Control: no-store, no-cache, must-revalidate, max-age=0', 'replace' => true,
+                'status_code' => null],
             ['header' => 'Cache-Control: post-check=0, pre-check=0', 'replace' => false, 'status_code' => null],
             ['header' => 'Pragma: no-cache', 'replace' => true, 'status_code' => null],
             ['header' => 'HTTP/1.1 200 ', 'replace' => true, 'status_code' => 200],
